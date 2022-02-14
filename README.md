2920fc367c9c4dac065a5637a5fe683a21c961d8
OLUSOLA ADEKUNLE <olusola.adekunle@outlook.com>
2022-02-14 12:17:19 +0100

    complete the frontend for the project

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.5.
 
+## Source Material
+https://www.bezkoder.com/angular-11-mongodb-node-js-express/#Angular_11_MongoDB_example_with_Nodejs_Express
+
+and 
+https://www.bezkoder.com/angular-11-crud-app/
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-
+import { Route, RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,8 +16,8 @@ import { TutorialsListComponent } from './components/tutorials-list/tutorials-li
     TutorialDetailsComponent,
     TutorialsListComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
